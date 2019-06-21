7691186346e109b930ec0ae839d3d813397fe2ae
Misty DeRamus <mistyderamus@gmail.com>
2019-06-21 10:43:38 -0500

    add object entries

--- main.js
+++ main.js
@@ -37,10 +37,19 @@ const outputElement = document.querySelector("#app")
 
 // Iterate the array of cars. Individual objects stored in `car`.
 allCars.forEach(car => {
-
+    
     // Iterate all of the values of the current car
     for (const value of Object.values(car)) {
-      outputElement.innerHTML += `<h2>${value}</h2>`
+        outputElement.innerHTML += `<h2>${value}</h2>`
+    }
+})
+
+outputElement.innerHTML += "<h1> Car List </h1>"
+
+allCars.forEach(car => {
+outputElement.innerHTML += "<hr>"
+    for (const entry of Object.entries(car)) {
+        outputElement.innerHTML += `<div> ${entry[0]}: ${entry[1]}</div>`
     }
 })
 
